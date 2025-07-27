1377784bfdc1b2a32c9dbf820a0cc102aebf1b97
Richard Ward <richard.ward637@outlook.com>
2025-07-27 22:50:35 +0100

    Add files via upload

--- megacommand.sh
+++ megacommand.sh
@@ -26,11 +26,12 @@ sudo apt update && sudo apt install -y \
   arduino avrdude dfu-util stm32flash openocd \
   libxcb-icccm4 libxcb-render-util0 libfuse2t64 \
   avahi-daemon avahi-discover \
+  terminator \
   gnome-software-plugin-flatpak && \
 
 # Flatpak setup and apps
 sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo && \
-flatpak install -y flathub io.github.scummvm.ScummVM org.libretro.RetroArch org.godotengine.Godot org.itch.Itch io.github.fastrun.ExtremeTuxRacer && \
+flatpak install -y flathub org.scummvm.ScummVM org.libretro.RetroArch org.godotengine.Godot org.itch.Itch io.github.fastrun.ExtremeTuxRacer && \
 
 # Snap apps
 sudo snap install code --classic && \
